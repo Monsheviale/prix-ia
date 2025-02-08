4291d880094bf6fae4d02f1e478247991987e2b8
Chris Monsheviale <chrismonsheviale0507@gmail.com>
2025-02-08 15:34:16 +0100

    modification surface

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ features_used = joblib.load("features_list.pkl")  # Charger les 244 colonnes uti
 st.title("🏡 Prédiction du Prix des Maisons")
 
 # Création des champs d'entrée pour les principales caractéristiques
-surface = st.number_input("Surface ", min_value=500, step=50)
+surface = st.number_input("Aire", min_value=500, step=50)
 
 chambres = st.number_input("Nombre de chambres", min_value=1, step=1)
 sdb = st.number_input("Nombre de salles de bain", min_value=1,  step=1)
